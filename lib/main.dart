a2632bdb0ffaab5b8939e0792296ecff225a0235
tkddn0518 <tkddn0518@naver.com>
2022-12-30 17:22:01 +0900

    - Scaffold 사용 - appBar, body, bottomNavigationBar 로 구분 - 커스텀 위젯 생성 방법 - ListView 사용법

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,68 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold( // 상, 중, 하로 나눠주는 위젯
+        appBar: AppBar(
+          actions: [Icon(Icons.search), Icon(Icons.library_books), Icon(Icons.add_alert)],
+          title: Text('금호동3가'),
+        ), //  상단: 앱바
+        body:Container(
+          child: Row(
+            children: [
+              Icon(Icons.camera),
+              Container(
+                width: 300,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('카메라 팝니다'),
+                    Text('주소'),
+                    Text('금액'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('4')
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
+            ),
+          ),
+        )
+      )
+    );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  const ShopItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Text('hi~'),
+        Text('hi~'),
+        Text('hi~'),
+        Text('hi~'),
+        Text('hi~'),
+      ],
+    );
   }
 }
